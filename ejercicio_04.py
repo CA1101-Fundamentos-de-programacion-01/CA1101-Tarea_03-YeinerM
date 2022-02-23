3a5f73dc78c882947c39c84857f295c094177e3e
YeinerM <99045921+YeinerM@users.noreply.github.com>
2022-02-23 12:49:54 -0600

    Update ejercicio_04.py

--- ejercicio_04.py
+++ ejercicio_04.py
@@ -1,8 +1,10 @@
-def suma_de_saltamones(n):
+def suma_del_saltamontes(n):
         sumatoria=0
         for i in range (n+1):
             sumatoria +=i
         print (sumatoria)
             
-(suma_de_saltamones(2))
-(suma_de_saltamones(8))
+        
+(suma_del_saltamontes(2))
+(suma_del_saltamontes(8))
+                      
