8da925b3ac248448eb40fa0167a054cd8812cf4e
YeinerM <99045921+YeinerM@users.noreply.github.com>
2022-02-23 12:31:12 -0600

    Update ejercicio_04.py

---
+++ ejercicio_04.py
@@ -0,0 +1,8 @@
+def suma_de_saltamones(n):
+        sumatoria=0
+        for i in range (n+1):
+            sumatoria +=i
+        print (sumatoria)
+            
+(suma_de_saltamones(2))
+(suma_de_saltamones(8))
